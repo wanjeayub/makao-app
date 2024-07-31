8153074ecd3215e33ea63cfd98972e97ca581d10
Wanje <ayub.wanje@outlook.com>
2024-08-01 02:05:33 +0300

    Add icons

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,12 @@
+import { FaQuestion, FaSearch, FaUserAlt} from 'react-icons/fa'
+
 export const Header = () => {
   return <header className="bg-slate-200 flex flex-row items-center justify-between max-w-4xl mx-auto p-3">
     <div><span>Makao</span></div>
+    <form className="bg-slate-100 p-2 rounded-lg items-center flex flex-row"><input className="bg-slate-100" type="text" placeholder="Search..." /><FaSearch className='text-slate-600'/></form>
     <div className="flex gap-1 items-center">
-      <form className="bg-slate-100 p-2 rounded-lg"><input className="bg-slate-100" type="text" placeholder="Search..." /></form>
-      <div>Account</div>
-      <div>Help</div>
+      <div className='flex flex-row items-center gap-1'><FaUserAlt className='text-slate-600'/>Account</div>
+      <div className='flex flex-row items-center'><FaQuestion className='text-slate-600'/>Help</div>
     </div>
     </header>;
 };
