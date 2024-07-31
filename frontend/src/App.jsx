83c9f3d1eb79ef3c9363ea02caf08a83c7539208
Wanje <ayub.wanje@outlook.com>
2024-08-01 01:48:59 +0300

    Add header elements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Header } from "./components/Header";
 import { Cart } from "./pages/Cart";
+import { Profile } from "./pages/Profile";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,3 +1,10 @@
 export const Header = () => {
-  return <div>Header</div>;
+  return <header className="bg-slate-200 flex flex-row items-center justify-between max-w-4xl mx-auto p-3">
+    <div><span>Makao</span></div>
+    <div className="flex gap-1 items-center">
+      <form className="bg-slate-100 p-2 rounded-lg"><input className="bg-slate-100" type="text" placeholder="Search..." /></form>
+      <div>Account</div>
+      <div>Help</div>
+    </div>
+    </header>;
 };
